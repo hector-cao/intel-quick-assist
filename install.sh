0124030dfe8be18888f20b81d29a35e368b88e77
Ubuntu <ubuntu@corsair-741103.maas>
2024-02-13 12:12:46 +0000

    update install scripts

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 set -e
+set -x
+
+# Install the 6.8 kernel
+UNAME_R=$(uname -r)
+if [[ $UNAME_R != 6.8* ]]; then
+    apt-add-repository ppa:canonical-kernel-team/ubuntu/unstable                                                 
+    # install kernel
+    apt install -y linux-image-6.8.0-5-generic
+    ### qat drivers are in modules-extra
+    apt install -y linux-modules-extra-6.8.0-5-generic
+    reboot
+fi
 
 # PPA
 add-apt-repository -y ppa:kobuk-team/qat-ubuntu
@@ -9,15 +21,21 @@ add-apt-repository -y ppa:kobuk-team/qat-ubuntu
 # 
 lspci -d :4940 -k
 
-# list QAT virtual devices (must enable SR_IOV 
-
+# qatlib
+apt install --yes qatengine qatlib-examples
 
 # qatengine
-apt install --yes qatengine
 
-# qatzip
+# check qatengine
+openssl engine -t -c -v qatengine
 
+# QAT engine algorithms
+#
 
-# BIOS : must enable VT-d
+# run qatengine
+openssl speed -elapsed -async_jobs 72 rsa2048
+openssl speed -engine qatengine -elapsed -async_jobs 72 rsa2048
 
-# Kernel : must add intel_iommu=on and iommu=pt
+openssl speed -elapsed -async_jobs 72 ecdhx25519
+openssl speed -elapsed -engine qatengine -elapsed ecdhx25519
+
