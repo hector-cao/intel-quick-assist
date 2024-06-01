27bd0786957d65c338d06779705ac0ddf3a0f7f3
Hector Cao <hector.cao@canonical.com>
2024-06-01 00:44:07 +0000

    update

--- install_standalone.sh
+++ install_standalone.sh
@@ -15,7 +15,7 @@ modprobe vfio-pci ids=8086:4941
 QAT_BDF=0000\:70\:00.0
 
 echo down > /sys/bus/pci/devices/$QAT_BDF/qat/state
-echo dc > /sys/bus/pci/devices/$QAT_BDF/qat/cfg_services
+echo "sym;asym" | tee /sys/bus/pci/devices/$QAT_BDF/qat/cfg_services
 echo up > /sys/bus/pci/devices/$QAT_BDF/qat/state
 
 echo 16 > /sys/bus/pci/devices/$QAT_BDF/sriov_numvfs
