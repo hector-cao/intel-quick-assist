9b2463e8f79c64ad49e60f2bff411cad9a2a5469
Hector Cao <hector.cao@canonical.com>
2024-07-08 18:44:09 +0000

    enable vfio

---
+++ enable_vfio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rmmod vfio-pci
+modprobe vfio-pci ids=8086:4941
+
+QAT_BDF=0000\:$1
+
+#echo down > /sys/bus/pci/devices/$QAT_BDF/qat/state
+#echo "sym;asym" | tee /sys/bus/pci/devices/$QAT_BDF/qat/cfg_services
+#echo up > /sys/bus/pci/devices/$QAT_BDF/qat/state
+
+# 16 vfs, other value not possible
+echo 16 > /sys/bus/pci/devices/$QAT_BDF/sriov_numvfs
